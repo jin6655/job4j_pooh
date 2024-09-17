44aed85a74d11e08ebb98f1fd4915137a3d68735
vasily <vasilypoiser@@yandex.ru>
2024-09-17 15:23:26 +0300

    commit 17/09/2024 01

--- src/main/java/ru/job4j/App.java
+++ src/main/java/ru/job4j/App.java
@@ -5,8 +5,6 @@ import java.util.Queue;
 
 public class App {
     public static void main(String[] args) {
-        String str = "";
-        int x = 2;
-        str = x > 3 ? "a" : "b";
+        System.out.println("!");
     }
 }
