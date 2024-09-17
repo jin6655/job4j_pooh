b1bddc24fa1a0637c12c161358234c05691dda72
vasily <vasilypoiser@@yandex.ru>
2024-09-17 17:39:05 +0300

    commit 17/09/2024 08

--- src/main/java/ru/job4j/App.java
+++ src/main/java/ru/job4j/App.java
@@ -5,8 +5,7 @@ import java.util.Queue;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("05");
-        System.out.println("05");
-        System.out.println("06");
+        System.out.println("!");
+
     }
 }
