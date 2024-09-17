54a58d056d60bcea308d6eb6543051e7ae7a7d55
vasily <vasilypoiser@@yandex.ru>
2024-09-17 20:40:11 +0300

    commit 17/09/2024 11

---
+++ src/main/java/ru/job4j/Calc.java
@@ -0,0 +1,16 @@
+package ru.job4j;
+
+import java.util.Random;
+
+public class Calc {
+
+    private int x;
+
+    public int calc() {
+        for (int i = 0; i < 10; i++) {
+            x += new Random().nextInt(100);
+        }
+        return x;
+    }
+
+}
